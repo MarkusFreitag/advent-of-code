68fd7f239abc8283f13cf9e67c91a3d7359574ed
Markus Freitag <fmarkus@mailbox.org>
2019-12-06 14:49:53 +0100

    Add solutions for day 4 2019

---
+++ pkg/puzzles/2019_4.go
@@ -0,0 +1,83 @@
+package puzzles
+
+import (
+	"strconv"
+	"strings"
+)
+
+func includeDouble(num string) bool {
+	for i := 1; i < 10; i++ {
+		if strings.Contains(num, strings.Repeat(strconv.Itoa(i), 2)) {
+			return true
+		}
+	}
+	return false
+}
+
+func onlyDoubles(num string) bool {
+	var ok bool
+	for i := 1; i < 10; i++ {
+		if strings.Contains(num, strings.Repeat(strconv.Itoa(i), 2)) && !strings.Contains(num, strings.Repeat(strconv.Itoa(i), 3)) {
+			ok = true
+		}
+	}
+	return ok
+}
+
+func onlyIncreasing(num string) bool {
+	for idx, i := range num {
+		if idx == len(num)-1 {
+			break
+		}
+		n, _ := strconv.Atoi(string(i))
+		m, _ := strconv.Atoi(string(num[idx+1]))
+		if n > m {
+			return false
+		}
+	}
+	return true
+}
+
+type y2019d4p1 struct{}
+
+func (p *y2019d4p1) Solve(input string) (string, error) {
+	parts := strings.Split(input, "-")
+	min, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return "", err
+	}
+	max, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return "", err
+	}
+	numbers := make([]string, 0)
+	for i := min; i <= max; i++ {
+		num := strconv.Itoa(i)
+		if includeDouble(num) && onlyIncreasing(num) {
+			numbers = append(numbers, num)
+		}
+	}
+	return strconv.Itoa(len(numbers)), nil
+}
+
+type y2019d4p2 struct{}
+
+func (p *y2019d4p2) Solve(input string) (string, error) {
+	parts := strings.Split(input, "-")
+	min, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return "", err
+	}
+	max, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return "", err
+	}
+	numbers := make([]string, 0)
+	for i := min; i <= max; i++ {
+		num := strconv.Itoa(i)
+		if onlyDoubles(num) && onlyIncreasing(num) {
+			numbers = append(numbers, num)
+		}
+	}
+	return strconv.Itoa(len(numbers)), nil
+}
---
+++ pkg/puzzles/2019_4_test.go
@@ -0,0 +1,24 @@
+package puzzles
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIncludeDouble(t *testing.T) {
+	require.True(t, includeDouble("122"))
+	require.True(t, includeDouble("111"))
+	require.False(t, includeDouble("101"))
+}
+
+func TestOnlyDoubles(t *testing.T) {
+	require.True(t, onlyDoubles("122"))
+	require.False(t, onlyDoubles("111"))
+	require.False(t, onlyDoubles("101"))
+}
+
+func TestOnlyIncreasing(t *testing.T) {
+	require.True(t, onlyIncreasing("149"))
+	require.False(t, onlyIncreasing("1428"))
+}
