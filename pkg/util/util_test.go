375fc90a6957e630e2a6fb279657c6f87c574b42
Markus Freitag <fmarkus@mailbox.org>
2019-12-06 23:58:31 +0100

    Add tests for util pkg

---
+++ pkg/util/util_test.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestInputFilename(t *testing.T) {
+  require.Equal(t, "inputs/2019_1.txt", inputFilename(2019, 1))
+}
+
+func TestStrInSlice(t *testing.T) {
+	require.True(t, StrInSlice("3", []string{"1", "2", "3"}))
+	require.False(t, StrInSlice("4", []string{"1", "2", "3"}))
+}
+
+func TestIntInSlice(t *testing.T) {
+	require.True(t, IntInSlice(3, []int{1, 2, 3}))
+	require.False(t, IntInSlice(4, []int{1, 2, 3}))
+}
+
+func TestSum(t *testing.T) {
+	require.Equal(t, 5, Sum(5))
+	require.Equal(t, 5, Sum(2, 3))
+}
