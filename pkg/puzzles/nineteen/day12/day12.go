c75ad4ec1a77b023c82bb67ba1b7a66e3ebbe7bc
Markus Freitag <fmarkus@mailbox.org>
2019-12-12 08:49:44 +0100

    Please the linter

--- pkg/puzzles/nineteen/day12/day12.go
+++ pkg/puzzles/nineteen/day12/day12.go
@@ -1,141 +1,141 @@
 package day12
 
 import (
-  "fmt"
-  "math"
-  "strconv"
+	"fmt"
+	"math"
+	"strconv"
 	"strings"
 )
 
 func toNum(s string) int {
-  s = strings.TrimSpace(s)
-  num, _ := strconv.Atoi(s[2:])
-  return num
+	s = strings.TrimSpace(s)
+	num, _ := strconv.Atoi(s[2:])
+	return num
 }
 
 func parseLine(line string) (int, int, int) {
-  line = line[1:len(line)-1]
-  parts := strings.Split(line, ",")
-  x := toNum(parts[0])
-  y := toNum(parts[1])
-  z := toNum(parts[2])
-  return x, y, z
+	line = line[1 : len(line)-1]
+	parts := strings.Split(line, ",")
+	x := toNum(parts[0])
+	y := toNum(parts[1])
+	z := toNum(parts[2])
+	return x, y, z
 }
 
 type Point struct {
-  X, Y, Z int
+	X, Y, Z int
 }
 
 func (p *Point) Energy() int {
-  return int(math.Abs(float64(p.X))+math.Abs(float64(p.Y))+math.Abs(float64(p.Z)))
+	return int(math.Abs(float64(p.X)) + math.Abs(float64(p.Y)) + math.Abs(float64(p.Z)))
 }
 
 func (p *Point) String() string {
-  return fmt.Sprintf("<x=%3d,y=%3d,z=%3d>", p.X, p.Y, p.Z)
+	return fmt.Sprintf("<x=%3d,y=%3d,z=%3d>", p.X, p.Y, p.Z)
 }
 
 type Object struct {
-  Pos *Point
-  Vel *Point
+	Pos *Point
+	Vel *Point
 }
 
 func NewObject(x, y, z int) *Object {
-  return &Object{
-    Pos: &Point{X: x, Y: y, Z: z},
-    Vel: &Point{},
-  }
+	return &Object{
+		Pos: &Point{X: x, Y: y, Z: z},
+		Vel: &Point{},
+	}
 }
 
 func (o *Object) Accelerate() {
-  o.Pos.X += o.Vel.X
-  o.Pos.Y += o.Vel.Y
-  o.Pos.Z += o.Vel.Z
+	o.Pos.X += o.Vel.X
+	o.Pos.Y += o.Vel.Y
+	o.Pos.Z += o.Vel.Z
 }
 
 func (o *Object) KineticEnergy() int {
-  return o.Vel.Energy()
+	return o.Vel.Energy()
 }
 
 func (o *Object) PotentialEnergy() int {
-  return o.Pos.Energy()
+	return o.Pos.Energy()
 }
 
 func (o *Object) TotalEnergy() int {
-  return o.KineticEnergy()*o.PotentialEnergy()
+	return o.KineticEnergy() * o.PotentialEnergy()
 }
 
 func (o *Object) String() string {
-  return fmt.Sprintf("pos=%s, vel=%s", o.Pos.String(), o.Vel.String())
+	return fmt.Sprintf("pos=%s, vel=%s", o.Pos.String(), o.Vel.String())
 }
 
 type Universe []*Object
 
 func (u Universe) Evolve() {
-  for _, obj := range u {
-    fmt.Println(obj.String())
-  }
-  fmt.Print("\n")
-  // apply gravity
-  for idx, obj := range u {
-    for _, o := range u[idx:] {
-      applyGravity(obj, o)
-    }
-  }
-  // apply velocity
-  for _, obj := range u {
-    obj.Accelerate()
-  }
+	for _, obj := range u {
+		fmt.Println(obj.String())
+	}
+	fmt.Print("\n")
+	// apply gravity
+	for idx, obj := range u {
+		for _, o := range u[idx:] {
+			applyGravity(obj, o)
+		}
+	}
+	// apply velocity
+	for _, obj := range u {
+		obj.Accelerate()
+	}
 }
 
 func applyGravity(a, b *Object) {
-  if a.Pos.X < b.Pos.X {
-    a.Vel.X += 1
-    b.Vel.X -= 1
-  } else {
-    a.Vel.X -= 1
-    b.Vel.X += 1
-  }
-  if a.Pos.Y < b.Pos.Y {
-    a.Vel.Y += 1
-    b.Vel.Y -= 1
-  } else {
-    a.Vel.Y -= 1
-    b.Vel.Y += 1
-  }
-  if a.Pos.Z < b.Pos.Z {
-    a.Vel.Z += 1
-    b.Vel.Z -= 1
-  } else {
-    a.Vel.Z -= 1
-    b.Vel.Z += 1
-  }
+	if a.Pos.X < b.Pos.X {
+		a.Vel.X += 1
+		b.Vel.X -= 1
+	} else {
+		a.Vel.X -= 1
+		b.Vel.X += 1
+	}
+	if a.Pos.Y < b.Pos.Y {
+		a.Vel.Y += 1
+		b.Vel.Y -= 1
+	} else {
+		a.Vel.Y -= 1
+		b.Vel.Y += 1
+	}
+	if a.Pos.Z < b.Pos.Z {
+		a.Vel.Z += 1
+		b.Vel.Z -= 1
+	} else {
+		a.Vel.Z -= 1
+		b.Vel.Z += 1
+	}
 }
 
-type Part1 struct {}
+type Part1 struct{}
 
 func (p *Part1) Solve(input string) (string, error) {
-  input = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>"
+	input = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>"
 	lines := strings.Split(input, "\n")
-  universe := make(Universe, len(lines))
-  for idx, line := range lines {
-    universe[idx] = NewObject(parseLine(line))
-  }
-  for i:=0;i<11;i++ {
-    fmt.Printf("After %d steps:\n", i)
-    universe.Evolve()
-  }
-  var energy int
-  for _, obj := range universe {
-    fmt.Printf("pot: %3d + %3d + %3d = %4d", obj.Pos.X, obj.Pos.Y, obj.Pos.Z, obj.PotentialEnergy())
-    fmt.Printf("\tkin: %3d + %3d + %3d = %4d", obj.Vel.X, obj.Vel.Y, obj.Vel.Z, obj.KineticEnergy())
-    fmt.Printf("\ttotal: %4d * %4d = %4d\n", obj.PotentialEnergy(), obj.KineticEnergy(), obj.TotalEnergy())
-    energy += obj.TotalEnergy()
-  }
-  return strconv.Itoa(energy), nil
+	universe := make(Universe, len(lines))
+	for idx, line := range lines {
+		universe[idx] = NewObject(parseLine(line))
+	}
+	for i := 0; i < 11; i++ {
+		fmt.Printf("After %d steps:\n", i)
+		universe.Evolve()
+	}
+	var energy int
+	for _, obj := range universe {
+		fmt.Printf("pot: %3d + %3d + %3d = %4d", obj.Pos.X, obj.Pos.Y, obj.Pos.Z, obj.PotentialEnergy())
+		fmt.Printf("\tkin: %3d + %3d + %3d = %4d", obj.Vel.X, obj.Vel.Y, obj.Vel.Z, obj.KineticEnergy())
+		fmt.Printf("\ttotal: %4d * %4d = %4d\n", obj.PotentialEnergy(), obj.KineticEnergy(), obj.TotalEnergy())
+		energy += obj.TotalEnergy()
+	}
+	return strconv.Itoa(energy), nil
 }
 
-type Part2 struct {}
+type Part2 struct{}
 
 func (p *Part2) Solve(input string) (string, error) {
-  return "n/a", nil
+	return "n/a", nil
 }
--- pkg/puzzles/nineteen/day12/day12_test.go
+++ pkg/puzzles/nineteen/day12/day12_test.go
@@ -7,89 +7,90 @@ import (
 )
 
 func TestToNum(t *testing.T) {
-  require.Equal(t, 0, toNum("x=0"))
-  require.Equal(t, 10, toNum("y=10"))
-  require.Equal(t, -2, toNum("z=-2"))
+	require.Equal(t, 0, toNum("x=0"))
+	require.Equal(t, 10, toNum("y=10"))
+	require.Equal(t, -2, toNum("z=-2"))
 
-  require.Equal(t, 0, toNum(" x=0"))
-  require.Equal(t, 10, toNum(" y=10"))
-  require.Equal(t, -2, toNum(" z=-2"))
+	require.Equal(t, 0, toNum(" x=0"))
+	require.Equal(t, 10, toNum(" y=10"))
+	require.Equal(t, -2, toNum(" z=-2"))
 }
 
 func TestParseLine(t *testing.T) {
-  x, y, z := parseLine("<x=-1, y=0, z=2>")
-  require.Equal(t, -1, x)
-  require.Equal(t, 0, y)
-  require.Equal(t, 2, z)
+	x, y, z := parseLine("<x=-1, y=0, z=2>")
+	require.Equal(t, -1, x)
+	require.Equal(t, 0, y)
+	require.Equal(t, 2, z)
 
-  x, y, z = parseLine("<x=2, y=-10, z=-7>")
-  require.Equal(t, 2, x)
-  require.Equal(t, -10, y)
-  require.Equal(t, -7, z)
+	x, y, z = parseLine("<x=2, y=-10, z=-7>")
+	require.Equal(t, 2, x)
+	require.Equal(t, -10, y)
+	require.Equal(t, -7, z)
 
-  x, y, z = parseLine("<x=4, y=-8, z=8>")
-  require.Equal(t, 4, x)
-  require.Equal(t, -8, y)
-  require.Equal(t, 8, z)
+	x, y, z = parseLine("<x=4, y=-8, z=8>")
+	require.Equal(t, 4, x)
+	require.Equal(t, -8, y)
+	require.Equal(t, 8, z)
 
-  x, y, z = parseLine("<x=3, y=5, z=-1>")
-  require.Equal(t, 3, x)
-  require.Equal(t, 5, y)
-  require.Equal(t, -1, z)
+	x, y, z = parseLine("<x=3, y=5, z=-1>")
+	require.Equal(t, 3, x)
+	require.Equal(t, 5, y)
+	require.Equal(t, -1, z)
 }
 
 func TestPoint(t *testing.T) {
-  pt := &Point{X: -1, Y: 0, Z: 2}
-  require.Equal(t, 3, pt.Energy())
-  require.Equal(t, "<x= -1,y=  0,z=  2>", pt.String())
+	pt := &Point{X: -1, Y: 0, Z: 2}
+	require.Equal(t, 3, pt.Energy())
+	require.Equal(t, "<x= -1,y=  0,z=  2>", pt.String())
 
-  pt = &Point{X: 2, Y: -10, Z: -7}
-  require.Equal(t, 19, pt.Energy())
-  require.Equal(t, "<x=  2,y=-10,z= -7>", pt.String())
+	pt = &Point{X: 2, Y: -10, Z: -7}
+	require.Equal(t, 19, pt.Energy())
+	require.Equal(t, "<x=  2,y=-10,z= -7>", pt.String())
 
-  pt = &Point{X: 4, Y: -8, Z: 8}
-  require.Equal(t, 20, pt.Energy())
-  require.Equal(t, "<x=  4,y= -8,z=  8>", pt.String())
+	pt = &Point{X: 4, Y: -8, Z: 8}
+	require.Equal(t, 20, pt.Energy())
+	require.Equal(t, "<x=  4,y= -8,z=  8>", pt.String())
 
-  pt = &Point{X: 3, Y: 5, Z: -1}
-  require.Equal(t, 9, pt.Energy())
-  require.Equal(t, "<x=  3,y=  5,z= -1>", pt.String())
+	pt = &Point{X: 3, Y: 5, Z: -1}
+	require.Equal(t, 9, pt.Energy())
+	require.Equal(t, "<x=  3,y=  5,z= -1>", pt.String())
 }
 
 func TestObject(t *testing.T) {
-  obj := NewObject(-1, 0, 2)
-  require.Equal(t, -1, obj.Pos.X)
-  require.Equal(t, 0, obj.Pos.Y)
-  require.Equal(t, 2, obj.Pos.Z)
-  require.Equal(t, 0, obj.Vel.X)
-  require.Equal(t, 0, obj.Vel.Y)
-  require.Equal(t, 0, obj.Vel.Z)
+	obj := NewObject(-1, 0, 2)
+	require.Equal(t, -1, obj.Pos.X)
+	require.Equal(t, 0, obj.Pos.Y)
+	require.Equal(t, 2, obj.Pos.Z)
+	require.Equal(t, 0, obj.Vel.X)
+	require.Equal(t, 0, obj.Vel.Y)
+	require.Equal(t, 0, obj.Vel.Z)
 
-  obj.Accelerate()
-  require.Equal(t, -1, obj.Pos.X)
-  require.Equal(t, 0, obj.Pos.Y)
-  require.Equal(t, 2, obj.Pos.Z)
-  require.Equal(t, 0, obj.Vel.X)
-  require.Equal(t, 0, obj.Vel.Y)
-  require.Equal(t, 0, obj.Vel.Z)
+	obj.Accelerate()
+	require.Equal(t, -1, obj.Pos.X)
+	require.Equal(t, 0, obj.Pos.Y)
+	require.Equal(t, 2, obj.Pos.Z)
+	require.Equal(t, 0, obj.Vel.X)
+	require.Equal(t, 0, obj.Vel.Y)
+	require.Equal(t, 0, obj.Vel.Z)
 
-  obj.Vel.X = -1
-  obj.Vel.Y = 2
+	obj.Vel.X = -1
+	obj.Vel.Y = 2
 
-  obj.Accelerate()
-  require.Equal(t, -2, obj.Pos.X)
-  require.Equal(t, 2, obj.Pos.Y)
-  require.Equal(t, 2, obj.Pos.Z)
-  require.Equal(t, -1, obj.Vel.X)
-  require.Equal(t, 2, obj.Vel.Y)
-  require.Equal(t, 0, obj.Vel.Z)
+	obj.Accelerate()
+	require.Equal(t, -2, obj.Pos.X)
+	require.Equal(t, 2, obj.Pos.Y)
+	require.Equal(t, 2, obj.Pos.Z)
+	require.Equal(t, -1, obj.Vel.X)
+	require.Equal(t, 2, obj.Vel.Y)
+	require.Equal(t, 0, obj.Vel.Z)
 
-  require.Equal(t, 6, obj.PotentialEnergy())
-  require.Equal(t, 3, obj.KineticEnergy())
-  require.Equal(t, 18, obj.TotalEnergy())
+	require.Equal(t, 6, obj.PotentialEnergy())
+	require.Equal(t, 3, obj.KineticEnergy())
+	require.Equal(t, 18, obj.TotalEnergy())
 
-  require.Equal(t, "pos=<x= -2,y=  2,z=  2>, vel=<x= -1,y=  2,z=  0>", obj.String())
+	require.Equal(t, "pos=<x= -2,y=  2,z=  2>, vel=<x= -1,y=  2,z=  0>", obj.String())
 }
+
 /*
 func TestPart1(t *testing.T) {
 	p := &Part1{}
