5505aea9a56410e73e66da899c87579ad3fa15af
Markus Freitag <fmarkus@mailbox.org>
2020-12-11 13:09:41 +0100

    puzzles/year2020: Add solution for day 10 part 2

--- puzzles/year2020/day10/day10.go
+++ puzzles/year2020/day10/day10.go
@@ -37,6 +37,58 @@ func Part1(input string) (string, error) {
 	return strconv.Itoa(one * three), nil
 }
 
+type Node struct {
+	ID   int
+	Next map[*Node]bool
+}
+
 func Part2(input string) (string, error) {
-	return "n/a", nil
+	adapters := util.StrsToInts(strings.Split(input, "\n"))
+	sort.Ints(adapters)
+
+	start := &Node{ID: 0, Next: make(map[*Node]bool)}
+	end := &Node{ID: adapters[len(adapters)-1] + 3, Next: make(map[*Node]bool)}
+
+	nodes := make([]*Node, 0)
+	nodes = append(nodes, start)
+	for _, adapter := range adapters {
+		node := &Node{ID: adapter, Next: make(map[*Node]bool)}
+		for _, n := range nodes {
+			distance := node.ID - n.ID
+			if distance >= 1 && distance <= 3 {
+				n.Next[node] = true
+			}
+		}
+		nodes = append(nodes, node)
+	}
+
+	for _, n := range nodes {
+		distance := end.ID - n.ID
+		if distance >= 1 && distance <= 3 {
+			n.Next[end] = true
+		}
+	}
+
+	var counter int
+	for node := range start.Next {
+		counter += path(node, end)
+	}
+	return strconv.Itoa(counter), nil
+}
+
+var state = make(map[int]int)
+
+func path(node *Node, dest *Node) int {
+	if node.ID == dest.ID {
+		return 1
+	}
+	if v, ok := state[node.ID]; ok {
+		return v
+	}
+	var counter int
+	for n := range node.Next {
+		counter += path(n, dest)
+	}
+	state[node.ID] = counter
+	return counter
 }
--- puzzles/year2020/day10/day10_test.go
+++ puzzles/year2020/day10/day10_test.go
@@ -57,3 +57,55 @@ func TestPart1(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, "220", solution)
 }
+
+func TestPart2(t *testing.T) {
+	input := `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`
+	solution, err := Part2(input)
+	require.Nil(t, err)
+	require.Equal(t, "8", solution)
+
+	input = `28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`
+	solution, err = Part2(input)
+	require.Nil(t, err)
+	require.Equal(t, "19208", solution)
+}
