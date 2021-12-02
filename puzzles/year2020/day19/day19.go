cabc7fc7ca93e15e2751aab7c2fdc3bca02cb9ff
Markus Freitag <fmarkus@mailbox.org>
2021-12-02 20:50:21 +0100

    puzzles/year2020: Add solution for day 19

--- puzzles/year2020/day19/day19.go
+++ puzzles/year2020/day19/day19.go
@@ -111,57 +111,26 @@ func Part2(input string) (string, error) {
 		rules[id] = rule
 	*/
 
-	strs := strings.Split(blocks[1], "\n")
-	count := try1(strs)
-	//count := try2(strs)
-	//count := try3(strs)
+	rule42 := rules[42].Resolve()
+	rule31 := rules[31].Resolve()
+
+	rgx42 := regexp.MustCompile(fmt.Sprintf("^(%s)", rule42))
+	rgx31 := regexp.MustCompile(fmt.Sprintf("(%s)", rule31))
+
+	var count int
+	rgx := regexp.MustCompile(fmt.Sprintf("^(%s){2,}(%s)+$", rule42, rule31))
+	for _, str := range strings.Split(blocks[1], "\n") {
+		if rgx.MatchString(str) {
+			// Make sure that rule42 has more matches than rule31
+			count42, restStr := countMatches(str, rgx42)
+			count31, _ := countMatches(restStr, rgx31)
+			if count42 > count31 {
+				count++
+			}
+		}
+	}
 
 	return strconv.Itoa(count), nil
-}
-
-func try1(strs []string) int {
-	var count int
-	rgx := regexp.MustCompile(fmt.Sprintf("^(%s){2,}(%s)+$", rules[42].Resolve(), rules[31].Resolve()))
-	for _, str := range strs {
-		if rgx.MatchString(str) {
-			count++
-		}
-	}
-	return count
-}
-
-func try2(strs []string) int {
-	var count int
-	for _, str := range strs {
-		count42, restStr := countMatches(str, regexp.MustCompile(fmt.Sprintf("^(%s)", rules[42].Resolve())))
-		count31, _ := countMatches(restStr, regexp.MustCompile(fmt.Sprintf("(%s)", rules[31].Resolve())))
-		if count42 > count31 {
-			count++
-		}
-	}
-	return count
-}
-
-func try3(strs []string) int {
-	var count int
-	for _, str := range strs {
-		rgx42 := regexp.MustCompile(fmt.Sprintf("(%s)", rules[42].Resolve()))
-		rgx31 := regexp.MustCompile(fmt.Sprintf("(%s)", rules[31].Resolve()))
-
-		m42 := rgx42.FindAllStringIndex(str, -1)
-		if m42 == nil {
-			continue
-		}
-		m31 := rgx31.FindAllStringIndex(str[m42[len(m42)-1][1]:], -1)
-		if m31 == nil {
-			continue
-		}
-
-		if len(m42) > len(m31) {
-			count++
-		}
-	}
-	return count
 }
 
 func countMatches(str string, rgx *regexp.Regexp) (int, string) {
