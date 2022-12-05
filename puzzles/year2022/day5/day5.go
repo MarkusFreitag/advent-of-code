c93b2c1034a069cf6252f726eb4920b226e587ca
Markus Freitag <fmarkus@mailbox.org>
2022-12-05 06:42:18 +0100

    puzzles/year2022: add solution for day 5

---
+++ puzzles/year2022/day5/day5.go
@@ -0,0 +1,78 @@
+package day5
+
+import (
+	"strings"
+
+	"github.com/MarkusFreitag/advent-of-code/util"
+	"github.com/MarkusFreitag/advent-of-code/util/numbers"
+	"github.com/MarkusFreitag/advent-of-code/util/slice"
+)
+
+func parseInput(input string) ([][]string, []string) {
+	blocks := strings.Split(input, "\n\n")
+	stackLines := strings.Split(blocks[0], "\n")
+	instrLines := strings.Split(strings.TrimSpace(blocks[1]), "\n")
+
+	slice.Reverse(stackLines)
+
+	labels := slice.Chunks([]rune(stackLines[0]), 4)
+	stacks := make([][]string, len(labels))
+	for idx := range stacks {
+		stacks[idx] = make([]string, 0)
+	}
+
+	for _, line := range stackLines[1:] {
+		groups := slice.Chunks([]rune(line), 4)
+		for idx, group := range groups {
+			if numbers.Between(group[1], 65, 90) {
+				stacks[idx] = append(stacks[idx], string(rune(group[1])))
+			}
+		}
+	}
+
+	return stacks, instrLines
+}
+
+func parseInstruction(instr string) (int, int, int) {
+	fields := strings.Fields(instr)
+	return util.ParseInt(fields[1]),
+		util.ParseInt(fields[3]) - 1,
+		util.ParseInt(fields[5]) - 1
+}
+
+func Part1(input string) (string, error) {
+	stacks, instructions := parseInput(input)
+	for _, instr := range instructions {
+		count, from, to := parseInstruction(instr)
+		fromPile := stacks[from]
+		toPile := stacks[to]
+		crates, fromPile := slice.PopN(fromPile, count)
+		slice.Reverse(crates)
+		toPile = append(toPile, crates...)
+		stacks[from] = fromPile
+		stacks[to] = toPile
+	}
+	var top string
+	for _, pile := range stacks {
+		top += slice.Tail(pile, 1)[0]
+	}
+	return top, nil
+}
+
+func Part2(input string) (string, error) {
+	stacks, instructions := parseInput(input)
+	for _, instr := range instructions {
+		count, from, to := parseInstruction(instr)
+		fromPile := stacks[from]
+		toPile := stacks[to]
+		crates, fromPile := slice.PopN(fromPile, count)
+		toPile = append(toPile, crates...)
+		stacks[from] = fromPile
+		stacks[to] = toPile
+	}
+	var top string
+	for _, pile := range stacks {
+		top += slice.Tail(pile, 1)[0]
+	}
+	return top, nil
+}
