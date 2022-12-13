ed263f07af9f13de2bd03e2e75c55f2cb49600c2
Markus Freitag <fmarkus@mailbox.org>
2022-12-13 06:58:38 +0100

    puzzles/year2022: add solution for day 13

---
+++ puzzles/year2022/day13/day13_test.go
@@ -0,0 +1,43 @@
+package day13
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var testInput = `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`
+
+func TestPart1(t *testing.T) {
+	solution, err := Part1(testInput)
+	require.Nil(t, err)
+	require.Equal(t, "13", solution)
+}
+
+func TestPart2(t *testing.T) {
+	solution, err := Part2(testInput)
+	require.Nil(t, err)
+	require.Equal(t, "140", solution)
+}
--- puzzles/year2022/year2022.go
+++ puzzles/year2022/year2022.go
@@ -1,32 +1,34 @@
 package year2022
 
 import (
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day1"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day10"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day11"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day12"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day2"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day3"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day4"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day5"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day6"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day7"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day8"
-  "github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day9"
-  "github.com/MarkusFreitag/advent-of-code/util"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day1"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day10"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day11"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day12"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day13"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day2"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day3"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day4"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day5"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day6"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day7"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day8"
+	"github.com/MarkusFreitag/advent-of-code/puzzles/year2022/day9"
+	"github.com/MarkusFreitag/advent-of-code/util"
 )
 
 var Puzzles = map[string]util.Puzzle{
-	"day1":  { day1.Part1, day1.Part2 },
-	"day10":  { day10.Part1, day10.Part2 },
-	"day11":  { day11.Part1, day11.Part2 },
-	"day12":  { day12.Part1, day12.Part2 },
-	"day2":  { day2.Part1, day2.Part2 },
-	"day3":  { day3.Part1, day3.Part2 },
-	"day4":  { day4.Part1, day4.Part2 },
-	"day5":  { day5.Part1, day5.Part2 },
-	"day6":  { day6.Part1, day6.Part2 },
-	"day7":  { day7.Part1, day7.Part2 },
-	"day8":  { day8.Part1, day8.Part2 },
-	"day9":  { day9.Part1, day9.Part2 },
+	"day1":  {day1.Part1, day1.Part2},
+	"day10": {day10.Part1, day10.Part2},
+	"day11": {day11.Part1, day11.Part2},
+	"day12": {day12.Part1, day12.Part2},
+	"day13": {day13.Part1, day13.Part2},
+	"day2":  {day2.Part1, day2.Part2},
+	"day3":  {day3.Part1, day3.Part2},
+	"day4":  {day4.Part1, day4.Part2},
+	"day5":  {day5.Part1, day5.Part2},
+	"day6":  {day6.Part1, day6.Part2},
+	"day7":  {day7.Part1, day7.Part2},
+	"day8":  {day8.Part1, day8.Part2},
+	"day9":  {day9.Part1, day9.Part2},
 }
