fda3205fbbe91d8f62325ecc6b80d42f48952522
Markus Freitag <fmarkus@mailbox.org>
2022-12-16 10:51:20 +0100

    asd

--- puzzles/year2022/day16/day16.go
+++ puzzles/year2022/day16/day16.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/MarkusFreitag/advent-of-code/util"
 	"github.com/MarkusFreitag/advent-of-code/util/numbers"
+	"github.com/MarkusFreitag/advent-of-code/util/slice"
 )
 
 type Valve struct {
@@ -40,7 +41,6 @@ func parseInput(input string) map[string]*Valve {
 }
 
 func search(valves map[string]*Valve, current *Valve, opened map[string]*Valve, timeLeft, released int) int {
-	fmt.Printf("%s: %v\n", current.Name, util.Keys(opened))
 	tick := func(opened map[string]*Valve, timeLeft, released int) (int, int) {
 		timeLeft--
 		var sum int
@@ -52,19 +52,22 @@ func search(valves map[string]*Valve, current *Valve, opened map[string]*Valve, 
 	}
 
 	// open current valve
-	timeLeft, released = tick(opened, timeLeft, released)
-	if timeLeft <= 0 {
-		return released
+	if _, ok := opened[current.Name]; !ok {
+		timeLeft, released = tick(opened, timeLeft, released)
+		if timeLeft <= 0 {
+			return released
+		}
+		opened[current.Name] = current
 	}
-
-	opened[current.Name] = current
 
 	options := make([]*Valve, 0)
 	for _, valve := range current.Tunnels {
-		if _, ok := opened[valve.Name]; ok {
-			// skip if already opened before
-			continue
-		}
+		/*
+			if _, ok := opened[valve.Name]; ok {
+				// skip if already opened before
+				continue
+			}
+		*/
 		options = append(options, valve)
 	}
 	sort.Slice(options, func(i, j int) bool {
@@ -86,16 +89,65 @@ func search(valves map[string]*Valve, current *Valve, opened map[string]*Valve, 
 	return numbers.Max(results...)
 }
 
-func Part1(input string) (string, error) {
-	valves := parseInput(input)
+type searchFn func(map[string]*Valve, *Valve, int, []string) int
+
+func cached(fn searchFn) searchFn {
+	cache := make(map[string]int)
+
+	return func(valves map[string]*Valve, current *Valve, minutes int, opened []string) int {
+		key := fmt.Sprintf("%s|%d|%s", current.Name, minutes, strings.Join(opened, ","))
+		if val, found := cache[key]; found {
+			return val
+		}
+
+		result := fn(valves, current, minutes, opened)
+		cache[key] = result
+		fmt.Printf("cache size: %d\n", len(cache))
+		return result
+	}
+}
+
+func s(valves map[string]*Valve, current *Valve, minutes int, opened []string) int {
+	if minutes <= 0 {
+		return 0
+	}
+
 	var max int
-	for _, valve := range valves["AA"].Tunnels {
-		if released := search(valves, valve, make(map[string]*Valve, 0), 30, 0); released > max {
-			max = released
+	for _, valve := range current.Tunnels {
+		max = numbers.Max(max, s(valves, valve, minutes-1, opened))
+	}
+
+	if !slice.Contains(opened, current.Name) && current.Flow > 0 && minutes > 0 {
+		newOpened := make([]string, 0)
+		newOpened = append(newOpened, opened...)
+		newOpened = append(newOpened, current.Name)
+		minutes--
+		sum := minutes * current.Flow
+
+		for _, valve := range current.Tunnels {
+			max = numbers.Max(max, sum+s(valves, valve, minutes-1, newOpened))
 		}
 	}
-	return strconv.Itoa(max), nil
-	//return strconv.Itoa(search(valves, valves["AA"], make(map[string]*Valve, 0), 30, 0)), nil
+
+	return max
+}
+
+func Part1(input string) (string, error) {
+	valves := parseInput(input)
+	/*
+		var max int
+		for _, valve := range valves["AA"].Tunnels {
+			if released := search(valves, valve, make(map[string]*Valve, 0), 30, 0); released > max {
+				max = released
+			}
+		}
+		return strconv.Itoa(max), nil
+		//return strconv.Itoa(search(valves, valves["AA"], make(map[string]*Valve, 0), 30, 0)), nil
+	*/
+
+	cachedSearchFn := cached(s)
+
+	return strconv.Itoa(cachedSearchFn(valves, valves["AA"], 30, []string{})), nil
 }
 
 func Part2(input string) (string, error) {
