103a11d447ea96bba1822c0c973c86f488457605
Markus Freitag <fmarkus@mailbox.org>
2022-12-20 09:03:55 +0100

    asd

--- puzzles/year2022/day16/day16.go
+++ puzzles/year2022/day16/day16.go
@@ -2,7 +2,6 @@ package day16
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -40,81 +39,20 @@ func parseInput(input string) map[string]*Valve {
 	return valves
 }
 
-func search(valves map[string]*Valve, current *Valve, opened map[string]*Valve, timeLeft, released int) int {
-	tick := func(opened map[string]*Valve, timeLeft, released int) (int, int) {
-		timeLeft--
-		var sum int
-		for _, valve := range opened {
-			sum += valve.Flow
-		}
-		released = sum * timeLeft
-		return timeLeft, released
+func search(valves map[string]*Valve, current *Valve, minutes int, opened []string) int {
+	key := fmt.Sprintf("%s|%d|%s", current.Name, minutes, strings.Join(opened, ","))
+	if v, ok := cache[key]; ok {
+		return v
 	}
 
-	// open current valve
-	if _, ok := opened[current.Name]; !ok {
-		timeLeft, released = tick(opened, timeLeft, released)
-		if timeLeft <= 0 {
-			return released
-		}
-		opened[current.Name] = current
-	}
-
-	options := make([]*Valve, 0)
-	for _, valve := range current.Tunnels {
-		/*
-			if _, ok := opened[valve.Name]; ok {
-				// skip if already opened before
-				continue
-			}
-		*/
-		options = append(options, valve)
-	}
-	sort.Slice(options, func(i, j int) bool {
-		return options[i].Flow > options[j].Flow
-	})
-
-	results := make([]int, len(options)+1)
-	results = append(results, released)
-	for _, valve := range options {
-
-		// move to valve
-		timeLeft, released = tick(opened, timeLeft, released)
-		if timeLeft <= 0 {
-			return released
-		}
-
-		results = append(results, search(valves, valve, opened, timeLeft, released))
-	}
-	return numbers.Max(results...)
-}
-
-type searchFn func(map[string]*Valve, *Valve, int, []string) int
-
-func cached(fn searchFn) searchFn {
-	cache := make(map[string]int)
-
-	return func(valves map[string]*Valve, current *Valve, minutes int, opened []string) int {
-		key := fmt.Sprintf("%s|%d|%s", current.Name, minutes, strings.Join(opened, ","))
-		if val, found := cache[key]; found {
-			return val
-		}
-
-		result := fn(valves, current, minutes, opened)
-		cache[key] = result
-		fmt.Printf("cache size: %d\n", len(cache))
-		return result
-	}
-}
-
-func s(valves map[string]*Valve, current *Valve, minutes int, opened []string) int {
 	if minutes <= 0 {
+		cache[key] = 0
 		return 0
 	}
 
 	var max int
 	for _, valve := range current.Tunnels {
-		max = numbers.Max(max, s(valves, valve, minutes-1, opened))
+		max = numbers.Max(max, search(valves, valve, minutes-1, opened))
 	}
 
 	if !slice.Contains(opened, current.Name) && current.Flow > 0 && minutes > 0 {
@@ -125,29 +63,20 @@ func s(valves map[string]*Valve, current *Valve, minutes int, opened []string) i
 		sum := minutes * current.Flow
 
 		for _, valve := range current.Tunnels {
-			max = numbers.Max(max, sum+s(valves, valve, minutes-1, newOpened))
+			max = numbers.Max(max, sum+search(valves, valve, minutes-1, newOpened))
 		}
 	}
 
+	cache[key] = max
 	return max
 }
 
+var cache = make(map[string]int)
+
 func Part1(input string) (string, error) {
+	cache = make(map[string]int)
 	valves := parseInput(input)
-	/*
-		var max int
-		for _, valve := range valves["AA"].Tunnels {
-			if released := search(valves, valve, make(map[string]*Valve, 0), 30, 0); released > max {
-				max = released
-			}
-		}
-		return strconv.Itoa(max), nil
-		//return strconv.Itoa(search(valves, valves["AA"], make(map[string]*Valve, 0), 30, 0)), nil
-	*/
-
-	cachedSearchFn := cached(s)
-
-	return strconv.Itoa(cachedSearchFn(valves, valves["AA"], 30, []string{})), nil
+	return strconv.Itoa(search(valves, valves["AA"], 30, make([]string, 0))), nil
 }
 
 func Part2(input string) (string, error) {
--- puzzles/year2022/day16/day16_test.go
+++ puzzles/year2022/day16/day16_test.go
@@ -26,5 +26,5 @@ func TestPart1(t *testing.T) {
 func TestPart2(t *testing.T) {
 	solution, err := Part2(testInput)
 	require.Nil(t, err)
-	require.Equal(t, "", solution)
+	require.Equal(t, "1707", solution)
 }
