cea80edc78c8e0b0e5ddd4017f58ea3bc73d566e
Markus Freitag <fmarkus@mailbox.org>
2023-12-08 11:04:12 +0100

    util/maputil: add KeysFiltered and ValuesFiltered

--- util/maputil/map.go
+++ util/maputil/map.go
@@ -10,12 +10,32 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	return keys
 }
 
+func KeysFiltered[K comparable, V any](m map[K]V, fn func(k K) bool) []K {
+	keys := make([]K, 0)
+	for key := range m {
+		if fn(key) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
+}
+
 func Values[K comparable, V any](m map[K]V) []V {
 	values := make([]V, len(m))
 	var idx int
 	for _, value := range m {
 		values[idx] = value
 		idx++
+	}
+	return values
+}
+
+func ValuesFiltered[K comparable, V any](m map[K]V, fn func(v V) bool) []V {
+	values := make([]V, 0)
+	for _, value := range m {
+		if fn(value) {
+			values = append(values, value)
+		}
 	}
 	return values
 }
--- util/maputil/map_test.go
+++ util/maputil/map_test.go
@@ -13,7 +13,24 @@ func TestKeys(t *testing.T) {
 	require.ElementsMatch(t, []string{"a", "b", "c", "d"}, maputil.Keys(m))
 }
 
+func TestKeysFiltered(t *testing.T) {
+	m := map[string]any{"aa": 1, "ab": true, "bc": "a", "ad": 1.2}
+	fn := func(k string) bool {
+		return string(k[0]) == "a"
+	}
+	require.ElementsMatch(t, []string{"aa", "ab", "ad"}, maputil.KeysFiltered(m, fn))
+}
+
 func TestValues(t *testing.T) {
 	m := map[string]any{"a": 1, "b": true, "c": "a", "d": 1.2}
 	require.ElementsMatch(t, []any{1, true, "a", 1.2}, maputil.Values(m))
+}
+
+func TestValuesFiltered(t *testing.T) {
+	m := map[string]any{"a": 1, "b": true, "c": 2, "d": 1.2}
+	fn := func(v any) bool {
+		_, ok := v.(int)
+		return ok
+	}
+	require.ElementsMatch(t, []any{1, 2}, maputil.ValuesFiltered(m, fn))
 }
