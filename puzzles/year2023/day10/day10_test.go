54227844ad095aa31e1263c89a8402324bc6378d
Markus Freitag <fmarkus@mailbox.org>
2023-12-10 18:05:22 +0100

    puzzles/year2023: add solution for day 10

---
+++ puzzles/year2023/day10/day10_test.go
@@ -0,0 +1,79 @@
+package day10
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	input := `.....
+.S-7.
+.|.|.
+.L-J.
+.....`
+	solution, err := Part1(input)
+	require.Nil(t, err)
+	require.Equal(t, "4", solution)
+
+	input = `..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...`
+	solution, err = Part1(input)
+	require.Nil(t, err)
+	require.Equal(t, "8", solution)
+
+	input = `7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ`
+	solution, err = Part1(input)
+	require.Nil(t, err)
+	require.Equal(t, "8", solution)
+}
+
+func TestPart2(t *testing.T) {
+	input := `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`
+	solution, err := Part2(input)
+	require.Nil(t, err)
+	require.Equal(t, "4", solution)
+
+	input = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
+	solution, err = Part2(input)
+	require.Nil(t, err)
+	require.Equal(t, "8", solution)
+
+	input = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`
+	solution, err = Part2(input)
+	require.Nil(t, err)
+	require.Equal(t, "10", solution)
+}
