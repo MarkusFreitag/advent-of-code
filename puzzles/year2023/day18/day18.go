e78aa239252ccfbfd20d7e58b5c42ea63a220a80
Markus Freitag <fmarkus@mailbox.org>
2023-12-22 14:17:24 +0100

    follow up

--- puzzles/year2023/day18/day18.go
+++ puzzles/year2023/day18/day18.go
@@ -6,206 +6,198 @@ import (
 	"strings"
 
 	"github.com/MarkusFreitag/advent-of-code/util"
+	"github.com/MarkusFreitag/advent-of-code/util/numbers"
 )
 
-var (
-	neighs = [][2]int{
-		[2]int{-1, 0},
-		[2]int{0, 1},
-		[2]int{1, 0},
-		[2]int{0, -1},
+type Range [2][2]int
+
+func (r Range) Include(p [2]int) bool {
+	return (p[0] >= r[0][0] && p[0] <= r[1][0] && p[1] >= r[0][1] && p[1] <= r[1][1]) || (p[0] >= r[1][0] && p[0] <= r[0][0] && p[1] >= r[1][1] && p[1] <= r[0][1])
+}
+
+func (r Range) Len() int {
+	if r[0][0] == r[1][0] {
+		return numbers.Abs(r[0][1] - r[1][1])
 	}
-)
+	return numbers.Abs(r[0][0] - r[1][0])
+}
 
-func fill(grid [][]bool, pts, seen map[[2]int]bool, pt [2]int) [][]bool {
-	for _, dir := range neighs {
+func (r Range) String() string {
+	return fmt.Sprintf("%d|%d => %d|%d", r[0][0], r[0][1], r[1][0], r[1][1])
+}
+
+type Ranges []Range
+
+func (r Ranges) Include(p [2]int) int {
+	for idx, rng := range r {
+		if rng.Include(p) {
+			return idx
+		}
+	}
+	return -1
+}
+func (r Ranges) Len() int {
+	var total int
+	for _, rng := range r {
+		total += rng.Len()
+	}
+	return total
+}
+
+/*
+func count(minY, minX, maxY, maxX int, ranges Ranges, pt [2]int) int {
+	queue := make([][2]int, 0)
+	queue = append(queue, pt)
+	seen := make(map[[2]int]bool)
+	var count int
+	for len(queue) > 0 {
+		var item [2]int
+		item, queue = sliceutil.PopFront(queue)
+
+		if _, ok := seen[item]; ok {
+			continue
+		}
+		seen[item] = true
+
+		if ranges.Include(item) {
+			continue
+		}
+
+		count++
+
+		for _, dir := range [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
+			p := [2]int{item[0] + dir[0], item[1] + dir[1]}
+			if p[0] < minY || p[1] < minX || p[0] > maxY || p[1] > maxX {
+				continue
+			}
+			if _, ok := seen[p]; ok {
+				continue
+			}
+			if ranges.Include(p) {
+				continue
+			}
+			queue = append(queue, p)
+		}
+	}
+	return count
+}
+
+func startingPoint(minY, minX, maxY, maxX int, ranges Ranges, pt [2]int) [2]int {
+	for _, dir := range [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}} {
 		p := [2]int{pt[0] + dir[0], pt[1] + dir[1]}
-		if p[0] < 0 || p[1] < 0 || p[0] >= len(grid) || p[1] >= len(grid[0]) {
+		if ranges.Include(p) {
 			continue
 		}
-		if _, ok := seen[p]; ok {
+		if p[0] < minY || p[1] < minX || p[0] > maxY || p[1] > maxX {
 			continue
 		}
-		if _, ok := pts[p]; ok {
-			continue
-		}
-		seen[p] = true
-		grid[p[0]][p[1]] = true
-		grid = fill(grid, pts, seen, p)
+		return p
 	}
-	return grid
+	return pt
+}
+*/
+
+func parseInput(input string, fn func(string) (string, int)) (int, int, int, int, Ranges, Ranges) {
+	minY, minX := numbers.MaxInteger, numbers.MaxInteger
+	maxY, maxX := 0, 0
+	pos := [2]int{0, 0}
+	lines := strings.Split(input, "\n")
+	vertRanges, horiRanges := make(Ranges, 0), make(Ranges, 0)
+	for _, line := range lines {
+		dir, meter := fn(line)
+		var newPos [2]int
+		switch dir {
+		case "U":
+			newPos = [2]int{pos[0] - meter, pos[1]}
+		case "R":
+			newPos = [2]int{pos[0], pos[1] + meter}
+		case "D":
+			newPos = [2]int{pos[0] + meter, pos[1]}
+		case "L":
+			newPos = [2]int{pos[0], pos[1] - meter}
+		}
+		if dir == "U" || dir == "D" {
+			vertRanges = append(vertRanges, Range{pos, newPos})
+		}
+		if dir == "R" || dir == "L" {
+			horiRanges = append(horiRanges, Range{pos, newPos})
+		}
+		pos = newPos
+		minY = numbers.Min(minY, pos[0])
+		minX = numbers.Min(minX, pos[1])
+		maxY = numbers.Max(maxY, pos[0])
+		maxX = numbers.Max(maxX, pos[1])
+	}
+	return minY, minX, maxY, maxX, vertRanges, horiRanges
+}
+
+/*
+#######
+#.....#
+###...#
+..#...#
+..#...#
+###.###
+#...#..
+##..###
+.#....#
+.######
+*/
+
+func count(minY, minX, maxY, maxX int, vertRanges, horiRanges Ranges) int {
+	var count int
+	for y := minY; y <= maxY; y++ {
+		var inside bool
+		for x := minX; x <= maxX; x++ {
+			if vertRanges.Include([2]int{y, x}) != -1 {
+				inside = !inside
+
+				/*
+					if x-1 >= minX && vertRanges.Include([2]int{y, x - 1}) != -1 {
+						inside = true
+					}
+				*/
+
+				continue
+			} else if idx := horiRanges.Include([2]int{y, x}); idx != -1 {
+				x = numbers.Max(horiRanges[idx][0][1], horiRanges[idx][1][1])
+				continue
+			}
+
+			if inside {
+				count++
+			}
+		}
+	}
+	return count
 }
 
 func Part1(input string) (string, error) {
-	/*
-		minY, minX := numbers.MaxInteger, numbers.MaxInteger
-		maxY, maxX := 0, 0
-		pos := [2]int{0, 0}
-		for _, line := range strings.Split(input, "\n") {
-			fields := strings.Fields(line)
-			switch fields[0] {
-			case "U":
-				pos[0] = pos[0] - util.ParseInt(fields[1])
-			case "R":
-				pos[1] = pos[1] + util.ParseInt(fields[1])
-			case "D":
-				pos[0] = pos[0] + util.ParseInt(fields[1])
-			case "L":
-				pos[1] = pos[1] - util.ParseInt(fields[1])
-			}
-
-			minY = numbers.Min(minY, pos[0])
-			minX = numbers.Min(minX, pos[1])
-			maxY = numbers.Max(maxY, pos[0])
-			maxX = numbers.Max(maxX, pos[1])
-
-		}
-		fmt.Printf("%d|%d => %d|%d\n", minY, minX, maxY, maxX)
-	*/
-	pos := [2]int{390, 90}
-	grid := make([][]bool, 500)
-	for idx := range grid {
-		grid[idx] = make([]bool, 300)
-	}
-	grid[390][90] = true
-	points := make(map[[2]int]bool)
-	points[[2]int{390, 90}] = true
-	for _, line := range strings.Split(input, "\n") {
+	minY, minX, maxY, maxX, vertRanges, horiRanges := parseInput(input, func(line string) (string, int) {
 		fields := strings.Fields(line)
-		switch fields[0] {
-		case "U":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[0] = pos[0] - 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "R":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[1] = pos[1] + 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "D":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[0] = pos[0] + 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "L":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[1] = pos[1] - 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		}
-	}
+		meter := util.ParseInt(fields[1])
+		return fields[0], meter
+	})
 
-	grid = fill(grid, points, make(map[[2]int]bool), [2]int{390, 90})
-
-	for _, row := range grid {
-		var line string
-		for _, cell := range row {
-			if cell {
-				line += "#"
-			} else {
-				line += "."
-			}
-		}
-		fmt.Println(line)
-	}
-
-	var count int
-	for _, row := range grid {
-		for _, cell := range row {
-			if !cell {
-				count++
-			}
-		}
-	}
-	return strconv.Itoa(count + len(points)), nil
+	return strconv.Itoa(count(minY, minX, maxY, maxX, vertRanges, horiRanges) + vertRanges.Len() + horiRanges.Len()), nil
+	/*
+		start := startingPoint(minY, minX, maxY, maxX, ranges, [2]int{0, 0})
+		return strconv.Itoa(count(minY, minX, maxY, maxX, ranges, start) + ranges.Len()), nil
+	*/
 }
 
 func Part2(input string) (string, error) {
-	/*
-		minY, minX := numbers.MaxInteger, numbers.MaxInteger
-		maxY, maxX := 0, 0
-		pos := [2]int{0, 0}
-		for _, line := range strings.Split(input, "\n") {
-			fields := strings.Fields(line)
-
-			hex := strings.TrimPrefix(strings.TrimSuffix(strings.TrimPrefix(fields[2], "("), ")"), "#")
-			dir, _ := strconv.ParseInt(hex[5:], 16, 64)
-			meters, _ := strconv.ParseInt(hex[:5], 16, 64)
-
-			fmt.Printf("%s %d %d\n", hex, meters, dir)
-
-			switch dir {
-			case 3:
-				pos[0] = pos[0] - int(meters)
-			case 0:
-				pos[1] = pos[1] + int(meters)
-			case 1:
-				pos[0] = pos[0] + int(meters)
-			case 2:
-				pos[1] = pos[1] - int(meters)
-			}
-
-			minY = numbers.Min(minY, pos[0])
-			minX = numbers.Min(minX, pos[1])
-			maxY = numbers.Max(maxY, pos[0])
-			maxX = numbers.Max(maxX, pos[1])
-
-		}
-		fmt.Printf("%d|%d => %d|%d\n", minY, minX, maxY, maxX)
-		return "na", nil
-	*/
-	pos := [2]int{19141420, 4698860}
-	grid := make([][]bool, 19141420+623860)
-	for idx := range grid {
-		grid[idx] = make([]bool, 4698860+13464692)
-	}
-	grid[19141420][4698860] = true
-	points := make(map[[2]int]bool)
-	points[[2]int{19141420, 4698860}] = true
-	for _, line := range strings.Split(input, "\n") {
+	minY, minX, maxY, maxX, vertRanges, horiRanges := parseInput(input, func(line string) (string, int) {
+		dirMap := map[int]string{3: "U", 0: "R", 1: "D", 2: "L"}
 		fields := strings.Fields(line)
-		switch fields[0] {
-		case "U":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[0] = pos[0] - 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "R":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[1] = pos[1] + 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "D":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[0] = pos[0] + 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		case "L":
-			for off := 0; off < util.ParseInt(fields[1]); off++ {
-				pos[1] = pos[1] - 1
-				grid[pos[0]][pos[1]] = true
-				points[pos] = true
-			}
-		}
-	}
+		dir, _ := strconv.ParseInt(fields[2][7:8], 16, 64)
+		meter, _ := strconv.ParseInt(fields[2][2:7], 16, 64)
+		return dirMap[int(dir)], int(meter)
+	})
 
-	grid = fill(grid, points, make(map[[2]int]bool), [2]int{390, 90})
-
-	var count int
-	for _, row := range grid {
-		for _, cell := range row {
-			if !cell {
-				count++
-			}
-		}
-	}
-	return strconv.Itoa(count + len(points)), nil
+	return strconv.Itoa(count(minY, minX, maxY, maxX, vertRanges, horiRanges)), nil
+	/*
+		start := startingPoint(minY, minX, maxY, maxX, ranges, [2]int{0, 0})
+		return strconv.Itoa(count(minY, minX, maxY, maxX, ranges, start) + ranges.Len()), nil
+	*/
 }
