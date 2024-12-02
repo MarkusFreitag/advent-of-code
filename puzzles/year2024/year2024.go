0f8f2cf636a359667fe06caccc6ed71672a1c57b
Markus Freitag <fmarkus@mailbox.org>
2024-12-02 06:49:27 +0100

    puzzles/year2024: add solution for day 2

--- puzzles/year2024/year2024.go
+++ puzzles/year2024/year2024.go
@@ -2,9 +2,11 @@ package year2024
 
 import (
   "github.com/MarkusFreitag/advent-of-code/puzzles/year2024/day1"
+  "github.com/MarkusFreitag/advent-of-code/puzzles/year2024/day2"
   "github.com/MarkusFreitag/advent-of-code/util"
 )
 
 var Puzzles = map[string]util.Puzzle{
 	"day1":  { day1.Part1, day1.Part2 },
+	"day2":  { day2.Part1, day2.Part2 },
 }
